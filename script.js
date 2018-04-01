f5ec16398e22864706eb6c16050f010a318d68d0
legiorex <batyushev@gmail.com>
2018-04-01 16:42:13 +1000

    Слушал уроки на https://www.youtube.com/user/PlurrimiTube разобрали переменные Мало что понимаю, но пока всё получается.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert ('Hello')
+document.write("Hello, World !");
+document.write("<br>Hello, World !");
+var number = 21;
+var string;
+string = "Hello World";
+var floatNumber = 1.23;
+var isTrue;
+isTrue = "true";
+document.write("Variable number = " + number + "<br />" + string + floatNumber + "<br />" + isTrue);
+var x = 5;
+var y;
+var result;
+y = 5;
+result = x+y;
+document.write(result);
+result = result + x;
+document.write("<br/>"+result);
